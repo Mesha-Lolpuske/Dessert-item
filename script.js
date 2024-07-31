3724868a8c5b67570cb5497681d9dd7fbe82b5ac
fatiha osman <fatihaosman17@gmail.com>
2024-07-31 04:04:15 -0700

    fatiha has commited

---
+++ script.js
@@ -0,0 +1,8 @@
+// Your users should be able to: 
+
+// - Add items to the cart and remove them
+// - Increase/decrease the number of items in the cart
+// - See an order confirmation modal when they click "Confirm Order"
+// - Reset their selections when they click "Start New Order"
+// - View the optimal layout for the interface depending on their device's screen size
+// - See hover and focus states for all interactive elements on the page
